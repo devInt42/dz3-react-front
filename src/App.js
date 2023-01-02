152188b655a7944a69773091619254f34fbeedae
Jmongle <110084761+Jmongle@users.noreply.github.com>
2023-01-02 11:49:18 +0900

    crud fetch 전

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-   const [hello, setHello] = useState('')
+    const baseUrl = "http://localhost:8080"
+//    const [hello, setHello] = useState('')
 
-    useEffect(() => {
+//     useEffect(() => {
       
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
+//         axios.get('/api/hello')
+//         .then(response => setHello(response.data))
+//         .catch(error => console.log(error))
+//     }, []);
+
+//     return (
+//         <div>
+//             백엔드에서 가져온 데이터입니다 : {hello}
+//         </div>
+//     );
+
+    const [user2, setUser2] = useState([]);
+    useEffect(()=>{
+        axios.get(baseUrl+'/user/userlist').then(response => setUser2(response.data)).catch(error => console.log(error))
     }, []);
 
-    return (
+    return(
         <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+            {user2.map((user2, i)=>{
+                return(
+                <div key={i}>
+                    <div>id : {user2.id}</div><br />
+                    <div>이름 : {user2.name}</div><br />
+                    <div>회사 : {user2.company}</div><br />
+                    <hr />
+                </div>
+                );
+            })}
+            
         </div>
     );
 }
