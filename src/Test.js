2c1b8cffcf31999b61696c648e763d8ce6c2ee92
Jihyeon <ajjh1025@gmail.com>
2023-01-02 17:27:59 +0900

    test

--- src/Test.js
+++ src/Test.js
@@ -3,18 +3,45 @@ import axios from 'axios';
 
 function Test() {
     const [hello,setHello] = useState('')
-
+    const baseUrl = "http://localhost:8080";
     useEffect( () => {
 
-        axios.get('/api/hello')
+        axios.get('/api/company/companyList')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
     }, []);
 
+    function searchOne(){
+        const url = baseUrl+"/api/company/3";
+        axios({
+            method:"get", url:url,
+        }).then((res)=> {
+            console.log(res.data)
+        }).catch((error)=> {
+            console.log(error);
+        })
+    }
+
+    function remove(){
+        const url = baseUrl+"/api/company/users/3";
+        axios({
+            method:"delete", url:url,
+        }).then((res)=>{
+            console.log("success");
+        }).catch((error)=>{
+            console.log(error);
+        })
+    }
+
     return (
         <>
-         백엔드에서 가져온 데이터입니다 : {hello}
+         백엔드에서 가져온 데이터입니다 : {JSON.stringify(hello)}
+         <div>
+            <button onClick={remove}>delete</button>
+            <button onClick={searchOne}>searchOne</button>
+        </div>
         </>
+        
     );
 }
 
