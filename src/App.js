79c3ff5fc990c756cb1d25ffa8a51c060813776c
Jihyeon <ajjh1025@gmail.com>
2023-01-03 13:44:43 +0900

    cc

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Homepage";
 import Employee from "./pages/Employee";
-import Company from "../src/pages/Company";
-
+import Company from "./pages/Company";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/dz3/">
+          <Route path="" element={<Home />} />
           <Route path="employee" element={<Employee/>}/>
-          <Route path="company" element={<Company />}/>
-          </Route>
+          <Route path="company" element={<Company/>}/>
+        </Route>
       </Routes>
     </Router>
   );
