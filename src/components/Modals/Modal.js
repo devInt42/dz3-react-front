a883d7bd9e5de121f7f8271a88aa65d27710ffdc
Jihyeon <ajjh1025@gmail.com>
2023-01-06 16:21:53 +0900

    pre loading

--- src/components/Modals/Modal.js
+++ src/components/Modals/Modal.js
@@ -6,49 +6,59 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import axios from "axios";
 import { BsTelephonePlus } from "react-icons/bs";
-
+import { BsFillFileEarmarkPersonFill } from "react-icons/bs";
 const Modal = (props) => {
   const { open, close, header } = props;
   const baseUrl = "http://localhost:8080";
   const [deptNameList, setDeptNameList] = useState([]);
   const [deptList, setDeptList] = useState([]);
+  const [deptDetail, setDeptDetail] = useState([]);
 
-  useEffect( ()=> {
-   
-        const url = baseUrl + "/department/deptnamelist";
-        axios({
-          method: "get",
-          url: url,
-        }).then((res) => {
-          setDeptNameList(res.data);
-        });
+  //부서 전체 출력
+  useEffect(() => {
+    const url = baseUrl + "/department/list";
+    axios({
+      method: "get",
+      url: url,
+    }).then((res) => {
+      setDeptNameList(res.data);
+    });
+  }, []);
 
-  },[]);
+  //부서값에 해당되는 직원 list로 출력하는 함수
+  function test() {
+    const url = baseUrl + "/department/list";
+    axios({
+      method: "get",
+      url: url,
+    }).then((res) => {
+      // console.log(res.data)
+      setDeptList(res.data);
+    });
+  }
 
-//부서값에 해당되는 직원 list로 출력하는 함수
-    function test() {
-
-        const url = baseUrl + "/department/deptnamelist";
-        axios({
-          method: "get",
-          url: url,
-        }).then((res) => {
-            // console.log(res.data)
-            setDeptList(res.data);
-        });
-
-    }
-    
-    // useEffect( ()=> {
-    //     const url = baseUrl + "/department/deptnamelist";
-    //     axios({
-    //         method: "get",
-    //         url: url,
-    //       }).then((res) => {
-    //         setDeptList(res.data);
-    //         console.log(res.data);
-    //       });
-    //     },[]);
+  //리스트값에 해당되는 직원의 상세페이지
+  function test2() {
+    const url = baseUrl + "/department/list";
+    axios({
+      method: "get",
+      url: url,
+    }).then((res) => {
+      // console.log(res.data)
+      setDeptDetail(res.data);
+      console.log(deptDetail);
+    });
+  }
+  // useEffect( ()=> {
+  //     const url = baseUrl + "/department/deptnamelist";
+  //     axios({
+  //         method: "get",
+  //         url: url,
+  //       }).then((res) => {
+  //         setDeptList(res.data);
+  //         console.log(res.data);
+  //       });
+  //     },[]);
 
   return (
     //open 누르면 openModal 클래스 생성
@@ -73,7 +83,12 @@ const Modal = (props) => {
                   {deptNameList.map((deptNameList, i) => {
                     return (
                       deptNameList && (
-                        <div key={i}>- <span onClick={ (() => test())}>{deptNameList.departmentName}</span></div>
+                        <div key={i}>
+                          -{" "}
+                          <span onClick={() => test()}>
+                            {deptNameList.departmentName}
+                          </span>
+                        </div>
                         // <div key={i}>- {deptNameList.departmentName}</div>
                       )
                     );
@@ -84,35 +99,70 @@ const Modal = (props) => {
                 </Col>
 
                 <Col sm={5} className="test2">
-                 {/* {JSON.stringify(deptList)} */}
-
-                 {deptList.map ( (deptList, i) => {
-                    return(
-                        deptList && (
-                            <div key={i}>
-
-                                <Container>
-                                    <Row>
-                                    <Col sm={3} > image </Col>
-                                    <Col sm={9}>
-                                        <Row className="name">위치 : {deptList.departmentLoc}</Row>
-                                        <Row className="stage">카테고리 : {deptList.departmentCategory}</Row>
-                                        <Row className="phnum"><div style={{width:'30px', marginRight:'5px'}}><BsTelephonePlus /></div>{deptList.departmentCall} </Row>
-                                     </Col>
-                                    </Row>
-                                <hr />
-                                </Container>
-                            </div>
-                        )
-                    )
-
-                 })}
-
+                  {/* {JSON.stringify(deptList)} */}
+                  {deptList.map((deptList, i) => {
+                    return (
+                      deptList && (
+                        <div key={i}>
+                          <Container>
+                            <Row onClick={() => test2()}>
+                              <Col sm={3} classname="image">
+                                {" "}
+                                <div style={{ padding: "25px" }}>
+                                  {" "}
+                                  <BsFillFileEarmarkPersonFill size="70" />
+                                </div>
+                              </Col>
+                              <Col sm={9}>
+                                <Row className="name">
+                                  위치 : {deptList.departmentLoc}
+                                </Row>
+                                <Row className="stage">
+                                  카테고리 : {deptList.departmentCategory}
+                                </Row>
+                                <Row className="phnum">
+                                  <div style={{ width: "35px" }}>
+                                    <BsTelephonePlus />
+                                  </div>
+                                  {deptList.departmentCall}{" "}
+                                </Row>
+                              </Col>
+                            </Row>
+                            <hr />
+                          </Container>
+                        </div>
+                      )
+                    );
+                  })}
                 </Col>
 
-                <Col sm={4} className="test3">
-                  값3
-                </Col>
+                {deptDetail && (
+                  <Col sm={4} className="test3">
+                    <div>
+                      <Row>
+                        <Col sm={9}>이미지1</Col>
+                        <Col sm={3}>이미지2</Col>
+                      </Row>
+                      <Row>
+                        {
+                          <div>
+                            {JSON.stringify(Object.values(deptDetail[0])[1])}
+                          </div>
+                        }
+                        {/* {JSON.stringify(Object.values(Object.values(deptDetail)))} */}
+                      </Row>
+                      <Row>a</Row>
+                      <Row>a</Row>
+                    </div>
+                    <hr />
+                    <div>
+                      <Row>a</Row>
+                      <Row>a</Row>
+                      <Row>a</Row>
+                      <Row>a</Row>
+                    </div>
+                  </Col>
+                )}
               </Row>
             </Container>
           </main>
@@ -121,7 +171,5 @@ const Modal = (props) => {
     </div>
   );
 };
-
-
 
 export default Modal;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,9 @@
 import { BsPersonCircle } from "react-icons/bs";
-import React, { useState } from 'react';
-import Modal from '../components/Modals/Modal.js'
-import axios from 'axios';
+import React, { useState } from "react";
+import Modal from "../components/Modals/Modal.js";
+import axios from "axios";
 
 const Home = () => {
-
   const [modalOpen, setModalOpen] = useState(false);
 
   const openModal = () => {
@@ -14,16 +13,14 @@ const Home = () => {
     setModalOpen(false);
   };
 
-
   return (
     <React.Fragment>
-    <BsPersonCircle onClick={openModal}></BsPersonCircle>
-    <Modal open={modalOpen} close={closeModal} header="조직도">
-     {/* children */}
-    </Modal>
-  </React.Fragment>
-
-  )
+      <BsPersonCircle size="30" onClick={openModal}></BsPersonCircle>
+      <Modal open={modalOpen} close={closeModal} header="조직도">
+        {/* children */}
+      </Modal>
+    </React.Fragment>
+  );
 };
 
 export default Home;
