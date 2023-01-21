6fb04d01e98e0b7fb6cb685d3f5876afb4dbd37e
integer <integer402@gmail.com>
2023-01-21 20:39:41 +0900

    권한 그룹 추가

--- src/components/authGroup/AddGroup.js
+++ src/components/authGroup/AddGroup.js
@@ -16,8 +16,11 @@ const AddGroup = () => {
   const baseUrl = "http://localhost:8080";
   const [companyList, setCompanyList] = useState();
   const [selectCompanySeq, setSelectCompanySeq] = useState();
-  const [inputCode, setInputCode] = useState(null);
-  const [inputName, setInputName] = useState(null);
+  const [inputCode, setInputCode] = useState("");
+  const [inputName, setInputName] = useState("");
+  const [returnCode, setReturnCode] = useState(null);
+  const [returnName, setReturnName] = useState(null);
+  const [disabled, setDisabled] = useState(false);
   // 회사 리스트 불러오기
   useEffect(() => {}, [selectCompanySeq]);
   const selectCompanyArea = async () => {
@@ -47,19 +50,91 @@ const AddGroup = () => {
   const changeAuthCode = (e) => {
     setInputCode(e.target.value);
   };
-  const changeAuthName = (e) => {
-    setInputName(e.target.value);
+  const changeAuthName = (e2) => {
+    setInputName(e2.target.value);
   };
+
+  useEffect(() => {
+    if (inputCode != "") codeCheck();
+  }, [inputCode]);
+
+  useEffect(() => {
+    if (inputName != "") nameCheck();
+  }, [inputName]);
+
+  const codeCheck = async () => {
+    let data = {
+      companySeq: selectCompanySeq,
+      authCode: inputCode,
+    };
+    try {
+      let codeRes = await axios.get(`${baseUrl}/auth/check`, {
+        params: data,
+      });
+      setReturnCode(codeRes.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const nameCheck = async () => {
+    let data = {
+      companySeq: selectCompanySeq,
+      authName: inputName,
+    };
+    try {
+      let nameRes = await axios.get(`${baseUrl}/auth/check`, {
+        params: data,
+      });
+      setReturnName(nameRes.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSubmit = async (event) => {
+    setDisabled(true);
+    event.preventDefault();
+    if (
+      (inputCode.length > 0 ? (returnCode > 0 ? false : true) : false) &&
+      (inputName.length > 0 ? (returnName > 0 ? false : true) : false)
+    ) {
+      let body = JSON.stringify({
+        companySeq: selectCompanySeq,
+        authCode: inputCode,
+        authName: inputName,
+      });
+      const headers = {
+        "Content-type": "application/json; charset=UTF-8",
+        Accept: "*/*",
+      };
+
+      axios
+        .post(`${baseUrl}/auth/add`, body, { headers })
+        .then((res) => {
+          alert("추가 되었습니다.");
+        })
+        .catch((e) => {
+          if (e.response && e.response.status === 500) {
+            return alert("실패했습니다.");
+          }
+        });
+
+      setDisabled(false);
+    } else {
+      alert("중복값으로 인해 그룹을 추가할 수 없습니다.");
+      setDisabled(false);
+    }
+  };
+
   const popover = (
     <Popover id="popover-basic">
       <Popover.Header as="h3">그룹 추가하기 </Popover.Header>
       <Popover.Body>
         <Container>
-          <Row>
+          <Form onSubmit={handleSubmit}>
             <Form.Select
               size="sm"
               onChange={changeSelectVal}
-              style={{ width: "200px" }}
+              style={{ width: "100%" }}
             >
               {companyList &&
                 companyList.map((companyMap, i) => (
@@ -68,18 +143,44 @@ const AddGroup = () => {
                   </option>
                 ))}
             </Form.Select>
-          </Row>
-          <Form>
             <Form.Group className="mb-3" controlId="inputAuthCode">
               <Form.Label>권한 코드</Form.Label>
-              <Form.Control type="text" onChange={changeAuthCode} />
+              <Form.Control
+                type="text"
+                autoComplete="off"
+                onChange={changeAuthCode}
+                isValid={
+                  inputCode.length > 0 ? (returnCode > 0 ? false : true) : false
+                }
+                isInvalid={
+                  inputCode.length > 0 ? (returnCode > 0 ? true : false) : true
+                }
+              />
+              <Form.Control.Feedback type="invalid">
+                중복된 권한코드입니다.
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="inputAuthName">
               <Form.Label>권한명</Form.Label>
-              <Form.Control type="text" onChange={changeAuthName} />
+              <Form.Control
+                type="text"
+                autoComplete="off"
+                onChange={changeAuthName}
+                isValid={
+                  inputName.length > 0 ? (returnName > 0 ? false : true) : false
+                }
+                isInvalid={
+                  inputName.length > 0 ? (returnName > 0 ? true : false) : true
+                }
+              />
+              <Form.Control.Feedback type="invalid">
+                중복된 권한명 입니다.
+              </Form.Control.Feedback>
             </Form.Group>
+            <button type="submit" id="sendGroupBtn" disabled={disabled}>
+              추가하기
+            </button>
           </Form>
-          <Button>추가하기</Button>
         </Container>
       </Popover.Body>
     </Popover>
