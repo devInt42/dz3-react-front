da601a7e53c48476851a6aa5859da5ba7681ccfe
xoxorbrb <tg3782@naver.com>
2023-01-24 18:51:58 +0900

    부서 리스트 사업장 열기/닫기 기능 구현

--- src/components/department/CompanyData.js
+++ src/components/department/CompanyData.js
@@ -1,42 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import WorkplaceData from "./WorkplaceData";
-import {HiOutlineBuildingOffice2} from 'react-icons/hi2'
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2'
 import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
 const CompanyData = () => {
     const baseUrl = "http://localhost:8080";
     const [company, setCompany] = useState([]);
     const [companyIsOpen, setCompanyIsOpen] = useState([]);
-    useEffect(()=> {
+    useEffect(() => {
         axios.get(`${baseUrl}/department/list/company`)
-        .then(res => setCompany(res.data))
-        .catch(error => console.log(error))
-    },[])
+            .then(res => setCompany(res.data))
+            .catch(error => console.log(error))
+    }, [])
 
-    const [toggleIcon, setToggleIcon] = useState([false]);
-    const icon = toggleIcon ? <HiChevronUp/>: <HiChevronDown/>
-    
+    const [toggleIcon, setToggleIcon] = useState([]);
+
+
+    useEffect(() => {
+        setToggleIcon(companyIsOpen);
+    }, [companyIsOpen])
 
     return (
         <div>
             {company && company.map((company, idx) => {
-               return(
-                <div onClick={() => {
-                    companyIsOpen.includes(idx) ? 
-                    setCompanyIsOpen(companyIsOpen.filter((company) => company !== idx)):
-                    setCompanyIsOpen([...companyIsOpen, idx])
-                    
-                    console.log((Object.values(companyIsOpen)));
-                }} key={idx}>
-                <HiOutlineBuildingOffice2 className = "companylist-icon"/>{company.companyCode}.{company.companyName}{icon}
-                    {companyIsOpen.includes(idx) && <WorkplaceData companySeq = {company.companySeq} key = {idx}/>}
-               </div>
-               )
+                return (
+                    <div key={idx}>
+                        <div onClick={() => {
+                            companyIsOpen.includes(idx) ?
+                                setCompanyIsOpen(companyIsOpen.filter((company) => company !== idx)) :
+                                setCompanyIsOpen([...companyIsOpen, idx])
+                        }}>
+                            <HiOutlineBuildingOffice2 className="companylist-icon" />{company.companyCode}.{company.companyName}
+                            {toggleIcon.includes(idx) ? <HiChevronDown /> : <HiChevronUp />}
+                        </div>
+                        {companyIsOpen.includes(idx) && <WorkplaceData companySeq={company.companySeq} key={idx} />}
+                    </div>
+                )
             })}
         </div>
     )
 
-    
+
 }
 
 export default CompanyData;
--- src/components/department/WorkplaceData.js
+++ src/components/department/WorkplaceData.js
@@ -1,28 +1,43 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import DepartmentData from "./DepartmentData";
-import {HiOutlineBuildingOffice} from 'react-icons/hi2';
+import { HiOutlineBuildingOffice } from 'react-icons/hi2';
 import "./css/DepartmentList.css";
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2'
+import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
 const WorkplaceData = (props) => {
 
     const [workplace, setWorkplace] = useState([]);
+    const [workplaceIsOpen, setWorkplaceIsOpen] = useState([]);
+    const [toggleIcon, setToggleIcon] = useState([]);
     const baseUrl = "http://localhost:8080";
-    useEffect(()=> {
+    useEffect(() => {
         axios.get(`${baseUrl}/department/list/workplace`)
-        .then(res => setWorkplace(res.data) )
-        .catch(error => console.log(error));
+            .then(res => setWorkplace(res.data))
+            .catch(error => console.log(error));
     }, [])
-    
+
+    useEffect(() => {
+        setToggleIcon(workplaceIsOpen);
+    }, [workplaceIsOpen])
+
     return (
         <>
             {workplace && workplace.map((workplace, idx) => {
-                return(
-                    <div key = {idx}>
+                return (
+                    <div key={idx}>
                         {workplace.companySeq === props.companySeq &&
-                            <div className = "workplacelist"><HiOutlineBuildingOffice className= "workplacelist-icon"
-                            />
-                            {workplace.workplaceCode}.{workplace.workplaceName}
-                                <DepartmentData workplaceSeq = {workplace.workplaceSeq}/>
+                            <div className="workplacelist">
+                                <div onClick = {() => {
+                        workplaceIsOpen.includes(idx) ? 
+                        setWorkplaceIsOpen(workplaceIsOpen.filter(workplace => workplace !== idx)) :
+                        setWorkplaceIsOpen([...workplaceIsOpen, idx]);
+                    }}>
+                                    <HiOutlineBuildingOffice className="workplacelist-icon" />
+                                    {workplace.workplaceCode}.{workplace.workplaceName}
+                                    {toggleIcon.includes(idx) ? <HiChevronDown /> : <HiChevronUp />}
+                                </div>
+                                {workplaceIsOpen.includes(idx) && <DepartmentData workplaceSeq={workplace.workplaceSeq} />}
                             </div>
                         }
                     </div>
