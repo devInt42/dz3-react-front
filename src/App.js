dcf02a0576f61de493caa3104eafb8839f980047
integer <integer402@gmail.com>
2023-01-26 10:03:47 +0900

    companyInsert 컴포넌트 삭제

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Home2 from "./pages/Home2";
 import Layout from "./pages/Layout";
 import MenuSet from "./pages/MenuSet";
 import CompanyDetail from "./components/company/CompanyDetail";
-import CompanyInsert from "./components/company/CompanyInsert";
 import CompanyUpdateform from "./components/company/CompanyUpdateform";
 import NoMenu from "./pages/NoMenu";
 import AuthGroup from "./pages/AuthGroup";
@@ -29,7 +28,6 @@ function App() {
           path="company/info/:companyCode"
           element={<CompanyDetail />}
         />
-        <Route exact path="company/insert" element={<CompanyInsert />} />
         <Route
           exact
           path="company/update/:companyCode"
--- src/components/authGroup/AuthMenu.js
+++ src/components/authGroup/AuthMenu.js
@@ -9,31 +9,7 @@ import { ReactComponent as FolderOpen } from "../authGroup/folderopen.svg";
 const AuthMenu = () => {
   const baseUrl = "http://localhost:8080";
   const [menuList, setMenuList] = useState([]);
-  const [checkItems, setCheckItems] = useState([]);
-
-  // 체크박스 단일 선택
-  const handleSingleCheck = (checked, id) => {
-    if (checked) {
-      // 단일 선택 시 체크된 아이템을 배열에 추가
-      setCheckItems((prev) => [...prev, id]);
-    } else {
-      // 단일 선택 해제 시 체크된 아이템을 제외한 배열 (필터)
-      setCheckItems(checkItems.filter((el) => el !== id));
-    }
-  };
-
-  // 체크박스 전체 선택
-  const handleAllCheck = (checked) => {
-    if (checked) {
-      // 전체 선택 클릭 시 데이터의 모든 아이템(id)를 담은 배열로 checkItems 상태 업데이트
-      const idArray = [];
-      menuList.forEach((el) => idArray.push(el.id));
-      setCheckItems(idArray);
-    } else {
-      // 전체 선택 해제 시 checkItems 를 빈 배열로 상태 업데이트
-      setCheckItems([]);
-    }
-  };
+  let checkedList = []; //값 저장
 
   // 전체 메뉴리스트 호출
   const getAllMenuList = async () => {
@@ -55,6 +31,14 @@ const AuthMenu = () => {
     getAllMenuList();
   }, []);
 
+  const changeCheckedList = (e) => {
+    if (checkedList.includes(e.target.value)) {
+      checkedList.pop(e.target.value);
+    } else {
+      checkedList.push(e.target.value.toString());
+    }
+    console.log(checkedList);
+  };
   return (
     <div style={{ border: "1px solid #f3f3f3" }}>
       <TreeView
@@ -75,7 +59,8 @@ const AuthMenu = () => {
                 style={{ marginTop: "5px" }}
                 name="checkVal"
                 value={menuItem.menuSeq}
-                id={menuItem.menuCode}
+                id={menuItem.menuSeq.toString()}
+                onChange={changeCheckedList}
               />
               <TreeItem
                 key={menuItem.menuSeq}
