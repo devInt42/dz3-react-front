28690edfbabcb2b8e513075ecd7794e7fcac5ee1
xoxorbrb <tg3782@naver.com>
2023-01-26 22:56:42 +0900

    부서클릭 시 회사이름, 사업장이름 출력

--- src/components/department/DepartmentDetail.js
+++ src/components/department/DepartmentDetail.js
@@ -16,45 +16,46 @@ const DepartmentDetail = (props) => {
     const [cWData, setCWData] = useState([]);
     const [companySeq, setCompanySeq] = useState(0);
     const [workplaceSeq, setWorkplaceSeq] = useState(0);
-    const [department, setDepartment] = useState([]);
+    const [department, setDepartment] = useState({});
 
-    // const getDepartment = async () => {
-    //     try {
-    //         const result = await axios.get(`${baseUrl}/department/list/${props.departmentSeq}`);
-    //         setDepartment(result);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-    // useEffect(() => {
-    //     if(props.departmentSeq != 0) {
-    //     getDepartment();
-    //     }
-    // },[props.departmentSeq])
-    // useEffect(() => {
-    //     console.log(department);
-    //     // if(props.departmentSeq != 0) {
-    //     //     let param = {
-    //     //         companySeq: props.companySeq,
-    //     //         workplaceSeq: props.workplaceSeq
-    //     //     }
-    //     //     axios.get(`${baseUrl}/department/list/name`, {
-    //     //         params: param
-    //     //     })
-    //     //     .then(res => setCWData(res.data))
-    //     //     .catch(error => console.log(error));
-    //     //     setCompanyName(Object.values(cWData)[0].companyName)
+    const getDepartment = async () => {
+        try {
+            const result = await axios.get(`${baseUrl}/department/list/${props.departmentSeq}`);
+            setDepartment(result.data[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        if (props.departmentSeq != 0) {
+            getDepartment();
+        }
+    }, [props.departmentSeq])
+    useEffect(() => {
+        console.log(department);
+        if (props.departmentSeq != 0) {
+            let param = {
+                companySeq: department.companySeq,
+                workplaceSeq: department.workplaceSeq
+            }
+            axios.get(`${baseUrl}/department/list/name`, {
+                params: param
+            })
+                .then(res => setCWData(res.data[0]))
+                .catch(error => console.log(error));
             
-    //     // }
-    // }, [department])
+            setCompanyName(cWData.companyName);
+            setWorkplaceName(cWData.workplaceName);
+        }
+    }, [department])
     return (
         <div>
             <div id="department-detail-header">
-                <b><BsDot/>부서 정보</b>
+                <b><BsDot />부서 정보</b>
                 <div>
                     <button>저장</button>
                     <button>삭제</button>
-                    <button id="department-detail-closebtn"><TfiClose/></button>
+                    <button id="department-detail-closebtn"><TfiClose /></button>
                 </div>
             </div>
             <hr />
@@ -62,43 +63,43 @@ const DepartmentDetail = (props) => {
                 <div className="department-detail-menu department-detail-basicmenu">기본 정보</div>
                 <div className="department-detail-menu">부서원 정보</div>
             </div>
-                <table id = "department-table">
-                    <thead></thead>
-                    <tbody>
-                        <tr>
-                            <th className = "department-table-title">회사</th>
-                            <td className = "department-table-content"></td>
-                        </tr>
-                        <tr>
-                            <th className = "department-table-title">사업장</th>
-                            <td className = "department-table-content">사업장이름</td>
-                        </tr>
-                        <tr>
-                            <th className = "department-table-title">상위부서</th>
-                            <td className = "department-table-content">상위부서 이름</td>
-                        </tr>
-                        <tr>
-                            <th className = "department-table-title">부서코드</th>
-                            <td className = "department-table-content">부서코드!!</td>
-                        </tr>
-                        <tr>
-                            <th className = "department-table-title">부서명</th>
-                            <td colSpan={3} className = "department-table-content"><Form.Control
-                                placeholder="부서 이름을 입력해주세요"
-                                Style="z-index: 0; background-color:#ffe9e9;"
-                            /></td>
-                        </tr>
-                        <tr>
-                            <th className = "department-table-title">부서주소</th>
-                            <td className = "department-table-content">부서주소</td>
-                        </tr>
-                        <tr>
-                            <th className = "department-table-title">사용여부</th>
-                            <td className = "department-table-content">사용 미사용</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <table id="department-table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <th className="department-table-title">회사</th>
+                        <td className="department-table-content">{companyName}</td>
+                    </tr>
+                    <tr>
+                        <th className="department-table-title">사업장</th>
+                        <td className="department-table-content">{workplaceName}</td>
+                    </tr>
+                    <tr>
+                        <th className="department-table-title">상위부서</th>
+                        <td className="department-table-content">상위부서 이름</td>
+                    </tr>
+                    <tr>
+                        <th className="department-table-title">부서코드</th>
+                        <td className="department-table-content">부서코드!!</td>
+                    </tr>
+                    <tr>
+                        <th className="department-table-title">부서명</th>
+                        <td colSpan={3} className="department-table-content"><Form.Control
+                            placeholder="부서 이름을 입력해주세요"
+                            Style="z-index: 0; background-color:#ffe9e9;"
+                        /></td>
+                    </tr>
+                    <tr>
+                        <th className="department-table-title">부서주소</th>
+                        <td className="department-table-content">부서주소</td>
+                    </tr>
+                    <tr>
+                        <th className="department-table-title">사용여부</th>
+                        <td className="department-table-content">사용 미사용</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
     )
 }
