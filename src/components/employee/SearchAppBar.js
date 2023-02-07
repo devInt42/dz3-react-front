ef2cc869cadd5272086d9850f2a1a0d4aa9ca4ff
Jmongle <110084761+Jmongle@users.noreply.github.com>
2023-02-07 09:32:36 +0900

    merge 전

--- src/components/employee/SearchAppBar.js
+++ src/components/employee/SearchAppBar.js
@@ -8,6 +8,12 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 export default function BasicTextFields() {
+
+  const [search, setSearch] = React.useState("");
+  const searchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <Box
       component="form"
@@ -31,7 +37,20 @@ export default function BasicTextFields() {
         </Select>
       </FormControl>
 
-      <TextField label="이름/메일/아이디" variant="outlined"  size="small" style={{marginLeft: "30px"}}/>
+      <FormControl size="small">
+        <InputLabel id="demo-simple-select-label">재직구분</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="재직구분"
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TextField label="이름/ID/Mail ID" variant="outlined"  size="small" value={search} onChange={searchChange}/>
       </Box>
   );
 }
