d830fa98409d2d4028ba67b3bf3eccc654c5f06e
xoxorbrb <tg3782@naver.com>
2023-02-14 20:49:26 +0900

    사원 관리 부서오류 수정중

--- src/components/departmentModal/ManageModal.js
+++ src/components/departmentModal/ManageModal.js
@@ -3,15 +3,20 @@ import DepartmentModal from "./DepartmentModal";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { Button } from "react-bootstrap";
+import axios from "axios";
 const ManageModal = (props) => {
   const [companySeq, setCompanySeq] = useState(null);
+  const baseUrl = "http://localhost:8080";
   useEffect(() => {
     setCompanySeq(props.companySeq);
   }, [props]);
   useEffect(() => {}, [companySeq]);
   //modal
   const [modalOpen, setModalOpen] = useState(false);
-
+  const departmentData = async (seq) => {
+    const result = axios.get(`${baseUrl}/department/list/${seq}`)
+    console.log(result);
+  }
   const openModal = () => {
     setModalOpen(true);
   };
@@ -34,6 +39,7 @@ const ManageModal = (props) => {
         Swal.fire("저장이 완료되었습니다.", "", "success", "#3085d6");
         setModalOpen(false);
         console.log(obj);
+        departmentData(obj.departmentSeq);
         props.updateIndexObject(props.idx, {
           companySeq: obj.companySeq,
           workplaceSeq: obj.workplaceSeq,
