fb4477191b366b5477120528aa46cb4a3b48733f
Jmongle <110084761+Jmongle@users.noreply.github.com>
2023-02-21 16:33:22 +0900

    failalert 수정

--- src/components/alert/SaveFailMenuAlert.js
+++ src/components/alert/SaveFailMenuAlert.js
@@ -4,13 +4,16 @@ import React, { useEffect, useRef, useState } from "react";
 
 function SaveFailMenuAlert(props) {
   const MySwal = withReactContent(Swal)
-MySwal.fire({
-  title:'필수 정보를 입력하지 않았습니다.',
-  text: '필수 정보를 입력해 주십시오.',
-  icon: 'error',
-  width: '600px',
-})
-props.setInputCheck(false);
+  useEffect(()=>{
+    MySwal.fire({
+      title:'필수 정보를 입력하지 않았습니다.',
+      text: '필수 정보를 입력해 주십시오.',
+      icon: 'error',
+      width: '600px',
+    })
+    props.setInputCheck(false);
+  },[props])
+ 
 }
 
 
